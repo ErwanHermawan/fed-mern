06696edcae94aa07739d2ce5dcd1be5225e51fce
ErwanHermawan <erwanhermawan858@gmail.com>
2023-06-23 14:17:16 +0700

    init

---
+++ src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/styled";
+
+const FlexBetween = styled(Box)({
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+});
+
+export default FlexBetween;
